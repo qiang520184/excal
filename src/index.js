d02c0249062982c92ae28897fef425b8f1a188a1
fdq <>
2019-11-08 09:47:42 +0800

    123

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Pages from './pages/index';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Pages} /> {/* 列表 */}
+				</Switch>
+			</BrowserRouter>
+		);
+	}
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
